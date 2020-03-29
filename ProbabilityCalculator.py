cc85123e2faaf65abbe809c9ad15d75445f66aee
Wei Xiang <boyboi86@users.noreply.github.com>
2020-03-29 14:31:24 +0800

    Update/ clean up comments

--- ProbabilityCalculator.py
+++ ProbabilityCalculator.py
@@ -23,14 +23,6 @@ p = print
 
 df = sampledata(period, tickers)
 
-#==============================================================================
-# Probability based on spot vs strike
-# for future option underlying is the future price itself
-# get ln return to derive ave rolling period std
-# finally annualized according to option leftover period
-# This however will underestimate volatility therefore just use VIX by 50%
-#==============================================================================
-
 def AnnualizedVolatility(window):
     df["LnReturn"] = np.log(df["^GSPC"]) - np.log(df["^GSPC"]).shift(1)
     df.dropna(inplace=True)
@@ -38,15 +30,6 @@ def AnnualizedVolatility(window):
     df.dropna(inplace=True)
     df["AdjVol"] = df["Volatility"] * np.sqrt(window)
     return df["AdjVol"][-1]
-    
-    
-
-
-#==============================================================================
-# some use ATM option IV = SPX VIX as input valid only for EOD
-# Rmb ppf 68% 95% 99.7% [1std, 2std, 3std]
-# Assume 365 days trading left on option
-#==============================================================================
 
 def ProbCalculate(spot, strike, window, sigma):
     sigma = 0.01 * sigma * np.sqrt(window / 365)
@@ -73,28 +56,12 @@ def CallProbCal(spot, strike, window):
     p("=" * 33)
     return ProbITM
 
-#==============================================================================
-# 2 cloest strike from the spot price
-# Width of strike is writer's closet short strikes from spot
-# The below works for both iron condor and vertical spread
-#==============================================================================
-    
 def PoPCalculator(premium, fees, Lstrike, Hstrike):
     width = Hstrike - Lstrike
     pop = 1 - (premium - fees) / width
     p("=" * 33)
     p(("Probability of Profit: {0} width: {1}").format(round(pop, 4), width))
     p("=" * 33)
-
-PoPCalculator(147.5, 0.26, 2600, 2800)
-#==============================================================================
-# Prob of profit formula for vertical spread credit
-# theortical price not valid for actual spread
-# put price and call price has to be retrieved from CBOE/ marketmaker for IV
-# Valid only for EOD since insuffice data
-# threshold is for min risk premium decimal 5% = 0.05
-# DTM = Date to maturity
-#==============================================================================
     
 def riskpremiumPut(premium, spot ,Lstrike, Hstrike, DTM, threshold):
     width = Hstrike - Lstrike 
