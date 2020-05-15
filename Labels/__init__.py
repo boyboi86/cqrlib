25ea57ef38485e289c27de3bcaa85f1ccdb30419
Wei Xiang <boyboi86@users.noreply.github.com>
2020-05-15 14:46:52 +0800

    include init py

---
+++ Labels/__init__.py
@@ -0,0 +1,6 @@
+try:
+    from research.Labels.triple_barrier_method import _pt_sl_t1, vert_bar, tri_bar, label, drop_label
+    from research.Labels.percentile_score import *
+except:
+    from Labels.triple_barrier_method import _pt_sl_t1, vert_bar, tri_bar, label, drop_label
+    from Labels.percentile_score import *
