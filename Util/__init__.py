81da11e7450a9e67e739ad5dacf17ab5e80ae32c
Wei Xiang <boyboi86@users.noreply.github.com>
2020-05-17 17:23:03 +0800

    included process jobs

--- Util/__init__.py
+++ Util/__init__.py
@@ -1,12 +1,12 @@
 try:
-    from research.Util.multiprocess import mp_pandas_obj
+    from research.Util.multiprocess import mp_pandas_obj, opt_num_threads, process_jobs_, process_jobs
     from research.Util.volatility import vol, parksinson_vol, garman_class_vol, yang_zhang_vol
     
     # Option related
     from research.Util.bsm_model import *
     from research.Util.opt_prob_calculator import * 
 except:
-    from Util.multiprocess import mp_pandas_obj
+    from Util.multiprocess import mp_pandas_obj, opt_num_threads, process_jobs_, process_jobs
     from Util.volatility import vol, parksinson_vol, garman_class_vol, yang_zhang_vol
     
     # Option related
