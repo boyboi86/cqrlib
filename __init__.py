73431b4ff87a68663d6d671e546a805c94f9ee65
Wei Xiang <boyboi86@users.noreply.github.com>
2020-05-17 17:40:41 +0800

    included MT_MC, wgth_rtn, process_jobs

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from research.Util.multiprocess import mp_pandas_obj
+from research.Util.multiprocess import mp_pandas_obj, opt_num_threads, process_jobs_, process_jobs
 from research.Util.volatility import vol, parksinson_vol, garman_class_vol, yang_zhang_vol
 from research.Util.bsm_model import *
 from research.Util.opt_prob_calculator import *
@@ -11,7 +11,7 @@ from research.Labels.percentile_score import *
 
 from research.Filters.filters import sym_csf
 
-#from research.Sampling.sample_unique import *
+from research.Sampling.sample_unique import co_events, seq_bts, idx_matrix, old_idx_matrix, MT_MC, wght_rtn
 
 from research.Features.fractional_diff import *
 from research.Features.futures_roll import *
