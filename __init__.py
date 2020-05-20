37b5cf3c5b41ecba9f272f1fac1c7c416af687c2
Wei Xiang <boyboi86@users.noreply.github.com>
2020-05-20 21:12:35 +0800

    include new imports

--- __init__.py
+++ __init__.py
@@ -1,17 +1,22 @@
+# make sure there is no mp func otherwise import individual func instead
+
 from research.Util.multiprocess import mp_pandas_obj, opt_num_threads, process_jobs_, process_jobs
-from research.Util.volatility import vol, parksinson_vol, garman_class_vol, yang_zhang_vol
+from research.Util.volatility import *
 from research.Util.bsm_model import *
 from research.Util.opt_prob_calculator import *
 
 #from research.Tools.root_methods import *
-#from research.Tools.Yahoo_Fin_data import YF_data
 
 from research.Labels.triple_barrier_method import _pt_sl_t1, vert_bar, tri_bar, label, drop_label
 from research.Labels.percentile_score import *
 
 from research.Filters.filters import sym_csf
 
-from research.Sampling.sample_unique import co_events, seq_bts, idx_matrix, old_idx_matrix, MT_MC, wght_by_rtn, wght_by_td
+from research.Sampling.sample_unique import (co_events, 
+                                             idx_matrix, mp_idx_matrix, 
+                                             seq_bts, mp_seq_bts, 
+                                             MC_seq_bts, MT_MC, 
+                                             wght_by_rtn, wght_by_td)
 
 from research.Features.fractional_diff import *
 from research.Features.futures_roll import *
