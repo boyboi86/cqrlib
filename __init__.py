6e1b328a5cc77223a8a3082da36c222c37b3c833
Wei Xiang <boyboi86@users.noreply.github.com>
2020-05-22 19:52:28 +0800

    import new func feat_imp

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ from research.Util.bsm_model import *
 from research.Util.opt_prob_calculator import *
 
 #from research.Tools.root_methods import *
-from research.Tools.stats_rpt import normality, unit_root, report_matrix, white_random
+from research.Tools.stats_rpt import normality, unit_root, report_matrix, white_random, feat_imp
 
 from research.Labels.triple_barrier_method import _pt_sl_t1, vert_bar, tri_bar, label, drop_label
 from research.Labels.percentile_score import *
