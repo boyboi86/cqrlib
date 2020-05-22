182e98982831ff95f66ccadf3f32f94184b4b6c6
Wei Xiang <boyboi86@users.noreply.github.com>
2020-05-22 11:53:09 +0800

    slight amendment to daily transact vol sample

--- Data_structure/standard_bars.py
+++ Data_structure/standard_bars.py
@@ -6,14 +6,25 @@ Can use yahoo data or retrieve single data call from utli folder
 import numpy as np
 import pandas as pd
 
-def dd_bars(df: pd.DataFrame, column: pd.Series, m: int):
+def transact_vol(data: pd.DataFrame, pct: float = 0.1):
+    bar_size = data.resample('1d').mean() * pct
+    return bar_size
+
+def dd_bars(data: pd.DataFrame, column: pd.Series, m: int = None):
+    '''
+    params: data => dataframe of close series
+    params: column => column of data sample; vol, dollar etc
+    '''
+    if m is None:
+        m = transact_vol(data = data)
+    
     t, ts, idx = column, 0, []
     for i, x in enumerate(t):
         ts += x
         if ts >= m:
             ts = 0; idx.append(i)
             continue
-    return df.iloc[idx]
+    return data.iloc[idx]
 
 def dollar_bar(df: pd.DataFrame, m: int):
     if 'DV' in df.columns:
