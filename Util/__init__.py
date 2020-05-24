874e6d4f66f6879e0743cfae861d54143c2c1d89
Wei Xiang <boyboi86@users.noreply.github.com>
2020-05-24 17:38:04 +0800

    include imports rname

--- Util/__init__.py
+++ Util/__init__.py
@@ -1,13 +1,14 @@
 try:
     from research.Util.multiprocess import mp_pandas_obj, opt_num_threads, process_jobs_, process_jobs
     from research.Util.volatility import vol, pks_vol, gc_vol, yz_vol
-    
+    from research.Util.indicator import bband_as_side
     # Option related
     from research.Util.bsm_model import *
     from research.Util.opt_prob_calculator import * 
 except:
     from Util.multiprocess import mp_pandas_obj, opt_num_threads, process_jobs_, process_jobs
     from Util.volatility import vol, pks_vol, gc_vol, yz_vol
+    from research.Util.indicator import bband_as_side
     
     # Option related
     from Util.bsm_model import *
