360749b3dd44b3247b962ba0287ef3b6dc2b47c8
Wei Xiang <boyboi86@users.noreply.github.com>
2020-05-25 17:05:28 +0800

    individual imports

--- Features/__init__.py
+++ Features/__init__.py
@@ -1,8 +1,8 @@
 try:
-    from research.Features.fractional_diff import *
-    from research.Features.futures_roll import *
-    from research.Features.PCA_weight_dist import *
+    from research.Features.fractional_diff import fracDiff_FFD, fracDiff, min_value, plot_min_ffd
+    #from research.Features.futures_roll import *
+    #from research.Features.PCA_weight_dist import *
 except:
-    from Features.fractional_diff import *
-    from Features.futures_roll import *
-    from Features.PCA_weight_dist import *
+    from Features.fractional_diff import fracDiff_FFD, fracDiff, min_value, plot_min_ffd
+    #from Features.futures_roll import *
+    #from Features.PCA_weight_dist import *
