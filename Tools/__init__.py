b86cc1edf5fac6d1e4e418fbf9f6c9a78a968413
Wei Xiang <boyboi86@users.noreply.github.com>
2020-06-01 11:22:11 +0800

    import from cv

--- Tools/__init__.py
+++ Tools/__init__.py
@@ -1,11 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu May 21 17:13:40 2020
-
-@author: Wei_X
-"""
-
 try:
     from research.Tools.stats_rpt import (normality, unit_root, report_matrix, white_random, feat_imp)
+    from research.Tools.cross_validate import (train_times, embargo_times, PurgedKFold, cv_score)
 except:
     from Tools.stats_rpt import (normality, unit_root, report_matrix, white_random, feat_imp)
+    from Tools.cross_validate import (train_times, embargo_times, PurgedKFold, cv_score)
