b1ccd381c2e8f2e2a912a2eba0f6acd76d1c01f1
Wei Xiang <boyboi86@users.noreply.github.com>
2020-06-01 14:23:52 +0800

    import new modules from metrics.py

--- Tools/__init__.py
+++ Tools/__init__.py
@@ -1,6 +1,8 @@
 try:
     from research.Tools.stats_rpt import (normality, unit_root, report_matrix, white_random, feat_imp)
     from research.Tools.cross_validate import (train_times, embargo_times, PurgedKFold, cv_score)
+    from research.Tools.metrics import (mdi, mda, sfi, plot_feat_imp)
 except:
     from Tools.stats_rpt import (normality, unit_root, report_matrix, white_random, feat_imp)
     from Tools.cross_validate import (train_times, embargo_times, PurgedKFold, cv_score)
+    from Tools.metrics import (mdi, mda, sfi, plot_feat_imp)
