ec59ccfca8a567fa0352a83161a28f494354ca1e
Wei Xiang <boyboi86@users.noreply.github.com>
2020-06-04 14:40:40 +0800

    include new imports/ base cls _feat_imp_analysis

--- Tools/__init__.py
+++ Tools/__init__.py
@@ -1,10 +1,10 @@
 try:
     from research.Tools.stats_rpt import (normality, unit_root, report_matrix, white_random, feat_imp)
     from research.Tools.cross_validate import (train_times, embargo_times, PurgedKFold, cv_score)
-    from research.Tools.metrics import (mdi, mda, sfi, mp_sfi, sample_weight_generator, plot_feat_imp)
+    from research.Tools.metrics import (mdi, mda, sfi, mp_sfi, sample_weight_generator, plot_feat_imp, _feat_imp_analysis, feat_imp_analysis)
     from research.Tools.feat_PCA import (o_feat, feat_pca)
 except:
     from Tools.stats_rpt import (normality, unit_root, report_matrix, white_random, feat_imp)
     from Tools.cross_validate import (train_times, embargo_times, PurgedKFold, cv_score)
-    from Tools.metrics import (mdi, mda, sfi, mp_sfi, sample_weight_generator, plot_feat_imp)
+    from Tools.metrics import (mdi, mda, sfi, mp_sfi, sample_weight_generator, plot_feat_imp, _feat_imp_analysis, feat_imp_analysis)
     from Tools.feat_PCA import (o_feat, feat_pca)
