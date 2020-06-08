6647f150cec8c55c4d96a0078909514b109640f4
Wei Xiang <boyboi86@users.noreply.github.com>
2020-06-09 00:32:55 +0800

    included make data min date = 1

--- Sample_data/make_data.py
+++ Sample_data/make_data.py
@@ -32,11 +32,15 @@ def create_price_data(start_price: float = 1000.00, mu: float = .0, var: float =
     X.cumsum().plot.line()
     return X.cumsum()
 
-def make_randomt1_data(n_samples: int =10000, max_days: float = 5.):
+def make_randomt1_data(n_samples: int =10000, max_days: float = 5., Bdate: bool = True):
     # generate a random dataset for a classification problem
+    if Bdate:
+        _freq = pd.tseries.offsets.BDay()
+    else:
+        _freq = 'D'
     _today = dt.datetime.today()
-    df0 = pd.date_range(periods=n_samples, freq=pd.tseries.offsets.BDay(), end=_today).to_numpy()
-    rand_days = np.random.uniform(max_days/2, max_days, n_samples)
+    df0 = pd.date_range(periods=n_samples, freq=_freq, end=_today)
+    rand_days = np.random.uniform(1, max_days, n_samples)
     rand_days = pd.Series([dt.timedelta(days = d) for d in rand_days], index = df0)
     X = pd.Series(df0 + pd.to_timedelta(rand_days, unit='d'), index = df0, name='t1').to_frame()
     X.sort_values(by='t1', inplace=True)
