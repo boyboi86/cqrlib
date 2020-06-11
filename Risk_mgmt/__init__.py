35cdb449aa1e3912ff9eedbf32f9b082ec8a6636
Wei Xiang <boyboi86@users.noreply.github.com>
2020-06-11 17:59:51 +0800

    include ef3m algo

--- Risk_mgmt/__init__.py
+++ Risk_mgmt/__init__.py
@@ -1,4 +1,6 @@
 try:
     from research.Risk_mgmt.bet_size import (avg_active_signals, get_signal, dynamic_bet, co_bets_size)
+    from research.Risk_mgmt.ef3m import mts_fit
 except:
     from Risk_mgmt.bet_size import (avg_active_signals, get_signal, dynamic_bet, co_bets_size)
+    from Risk_mgmt.ef3m import mts_fit
