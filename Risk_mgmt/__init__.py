4b3abbd83d1d896c1535ae502c4e096664d77bcc
Wei Xiang <boyboi86@users.noreply.github.com>
2020-06-13 15:51:30 +0800

    included new imports

--- Risk_mgmt/__init__.py
+++ Risk_mgmt/__init__.py
@@ -1,6 +1,6 @@
 try:
-    from research.Risk_mgmt.bet_size import (avg_active_signals, get_signal, dynamic_bet, co_bets_size)
-    from research.Risk_mgmt.ef3m import mts_fit
+    from research.Risk_mgmt.bet_size import (avg_active_signals, discrete_signal, get_signal, dynamic_bet, co_bets_size)
+    from research.Risk_mgmt.ef3m import mts_fit, m_bet_EF3M
 except:
-    from Risk_mgmt.bet_size import (avg_active_signals, get_signal, dynamic_bet, co_bets_size)
-    from Risk_mgmt.ef3m import mts_fit
+    from Risk_mgmt.bet_size import (avg_active_signals, discrete_signal, get_signal, dynamic_bet, co_bets_size)
+    from Risk_mgmt.ef3m import mts_fit, m_bet_EF3M
