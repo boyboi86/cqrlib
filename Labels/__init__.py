8b0bbcde991c5040e5ba203996a02650841eb4ba
Wei Xiang <boyboi86@users.noreply.github.com>
2020-06-16 22:53:49 +0800

    chang func name

--- Labels/__init__.py
+++ Labels/__init__.py
@@ -1,6 +1,6 @@
 try:
     from research.Labels.triple_barrier_method import _pt_sl_t1, vert_barrier, tri_barrier, meta_label, drop_label
-    from research.Labels.percentile_score import *
+    from research.Labels.percentile_score import rolling_percentileofscore
 except:
     from Labels.triple_barrier_method import _pt_sl_t1, vert_barrier, tri_barrier, meta_label, drop_label
-    from Labels.percentile_score import *
+    from Labels.percentile_score import rolling_percentileofscore
