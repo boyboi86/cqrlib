98fc2e3a1bcd1018e33cf91a5ab30ba6aaebd573
Wei Xiang <boyboi86@users.noreply.github.com>
2020-06-17 15:25:35 +0800

    included changes for yield and expenses

--- Sample_data/__init__.py
+++ Sample_data/__init__.py
@@ -1,4 +1,4 @@
 try:
-    from research.Sample_data.make_data import make_classification_data, create_price_data, make_randomt1_data
+    from research.Sample_data.make_data import make_classification_data, create_price_data, make_randomt1_data, create_portfolio
 except:
-    from Sample_data.make_data import make_classification_data, create_price_data, make_randomt1_data
+    from Sample_data.make_data import make_classification_data, create_price_data, make_randomt1_data, create_portfolio
--- Sample_data/make_data.py
+++ Sample_data/make_data.py
@@ -46,3 +46,36 @@ def make_randomt1_data(n_samples: int =10000, max_days: float = 5., Bdate: bool 
     df1.sort_values(inplace=True)
     X = pd.Series(df1, index = df0, name='t1').to_frame()
     return X
+
+def create_portfolio(price: list = [95, 1000], position: list = [1000, 10000], n_sample: int = 10000, imbalance: bool = True):
+    df = make_randomt1_data(n_samples = n_sample, max_days = 10., Bdate = True)
+    df['open'] = np.random.uniform(price[0], price[1], n_sample)
+    df['close'] = df['open'].apply(lambda x: x * np.random.uniform(0.8, 1.2))
+    df['open_pos'] = np.random.uniform(position[0], position[1], n_sample).round()
+    df['close_pos'] = np.random.uniform(position[0], position[1], n_sample).round()
+
+    df['yield'] = np.random.uniform(0.012, 0.12, n_sample)
+    df['expense'] = df['open'].apply(lambda x: x * np.random.uniform(0.005, 0.05))
+    df['label'] = np.nan
+    if imbalance:
+        for idx in df[df['open']<200].index:df.loc[idx,'label'] = 1
+    else:
+        for idx in df[df['open']<df['open'].quantile(0.4)].index:df.loc[idx,'label'] = 1
+    df['label'].fillna(0, inplace= True)
+    
+    df['clean_open'] = df['open'].apply(lambda x: x * np.random.uniform(0.92, 0.95))
+    df['clean_close'] = df['yield']
+    df['clean_close'] = df['clean_close'].apply(lambda x: 1-x).mul(df['close'])
+    df['yield'] = df['yield'].mul(df['close'])
+        
+
+    p_str = "Sample Portfolio Construct:\n{0}\nEquity label: {1}\nBond label: {2}\nEquity to Debt Ratio: {3:.4f}"
+    p(p_str.format("=" * 55,
+                   df['label'].value_counts()[0],
+                   df['label'].value_counts()[1], 
+                   df['label'].value_counts()[0]/df['label'].value_counts()[1]))
+    
+    junk_bond = df[(df['yield'] >= 0.1) & (df['label'] == 1)].count()[0]
+    div_equity = df[(df['yield'] >= 0.1) & (df['label'] != 1)].count()[0]
+    p("\nJunk bond (Below BBB-grade): {0} %\nDividend equity: {1} %".format(100 * junk_bond/ n_sample, 100 * div_equity/n_sample))
+    return df
